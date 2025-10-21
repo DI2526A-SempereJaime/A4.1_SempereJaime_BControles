2232aca7f260f8dd01370ff5b545786c1231bbbd
LordCardinal505 <jaimesempere63@gmail.com>
2025-10-21 12:10:06 +0200

    Add files via upload

---
+++ A4.1_MemeMaker/BoolToThickness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace A4._1_MemeMaker
+{
+    class BoolToThickness : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool borde = (bool)value;
+            if (borde)
+                return "10";
+            else
+                return "0";
+                
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
